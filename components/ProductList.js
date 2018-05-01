7e191d367a48a2586633a21f0f38dbb6737b0a03
unknown <leplan.ludwig@gmail.com>
2018-05-01 11:35:22 +0200

    feat() : add component modal

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,47 +1,57 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, List, ListItem, Text, Thumbnail, Right, Body, Left, View, Button, Form, Item, Input } from 'native-base';
-import {TouchableOpacity} from 'react-native'
+import { TouchableOpacity } from 'react-native'
+import Toast, { DURATION } from 'react-native-easy-toast'
+import message from '../constants/Message'
 import Modal from "react-native-modal";
 
 export default class DynamicListExample extends Component {
 
-    state = {
-     isModalVisible: false,
-     selectedProduct: {}
-    };
-  
+  state = {
+    isModalVisible: false,
+    selectedProduct: {}
+  };
+
+  async setNewVal() {
+    try {
+      const update = await AsyncStorage.setItem(this.state.selectedProduct.Nom, JSON.stringify(this.state.selectedProduct));
+      this.refs.toast.show(message.product.successUpdateProduct);
+    } catch (error) { }
+  }
+
 
   _toggleModal = () => this.setState({ isModalVisible: !this.state.isModalVisible });
 
   render() {
-    const {...items} = this.props
+    const { ...items } = this.props
     return (
-      <Container style={{marginTop: 10}}>
+      <Container style={{ marginTop: 10 }}>
         <Content>
           <List dataArray={items.data}
 
             renderRow={(item) =>
-             <ListItem onPress={this._toggleModal} avatar style={{marginBottom: 10}}>
-              <Left>
-                <Thumbnail  source={{ uri: 'https://preview.ibb.co/hJQO8c/451b9db3d936af95f168610422583c07.jpg' }} />
-              </Left>
-              <Body>
-                <Text>{item.Nom}</Text>
-                <Text note>Quantité : {item.Quantité}</Text>
-              </Body>
-            </ListItem>
+              <ListItem onPress={this._toggleModal} avatar style={{ marginBottom: 10 }}>
+                <Left>
+                  <Thumbnail source={{ uri: 'https://preview.ibb.co/hJQO8c/451b9db3d936af95f168610422583c07.jpg' }} />
+                </Left>
+                <Body>
+                  <Text>{item.Nom}</Text>
+                  <Text note>Quantité : {item.Quantité}</Text>
+                </Body>
+              </ListItem>
             }>
           </List>
         </Content>
+        <Toast position='top' ref="toast" />
         <Modal isVisible={this.state.isModalVisible}>
           <View style={{ flex: 0.5, backgroundColor: 'white' }}>
-          <Form>
-            <Item>
-              <Input  value={this.state.selectedProduct.Quantité} keyboardType='numeric' placeholder="Editer la quantité" />
-            </Item>
-          </Form>
-            <Button  onPress={this._toggleModal} warning><Text> Fermer </Text></Button>
-            <Button  onPress={this._toggleModal} primary><Text> Enregistrer </Text></Button>
+            <Form>
+              <Item>
+                <Input value={this.state.selectedProduct.Quantité} keyboardType='numeric' placeholder="Editer la quantité" />
+              </Item>
+            </Form>
+            <Button onPress={this._toggleModal} warning><Text> Fermer </Text></Button>
+            <Button onPress={this._toggleModal} primary><Text> Enregistrer </Text></Button>
           </View>
         </Modal>
       </Container>
--- constants/Message.js
+++ constants/Message.js
@@ -3,6 +3,7 @@ export default {
        successSaveProduct : 'Produit enregistré avec succés.',
        redondantProduct: 'Produit déja existant.',
        errorSaveProduct: 'Erreur lors de la sauvagarde du produit',
-       emptyFormString: 'Des champs obligatoires sont manquants'
+       emptyFormString: 'Des champs obligatoires sont manquants',
+       successUpdateProduct: 'Produit mis à jours avec succés.'
     }
 }
